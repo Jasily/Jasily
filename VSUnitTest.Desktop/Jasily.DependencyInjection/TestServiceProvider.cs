a28e965d5bf670daf89b0c3a1aa26e53b2a64bb4
赖巍 <skyoflw@gmail.com>
2016-11-21 02:55:57 +0800

    for compile.

--- VSUnitTest.Desktop/Jasily.DependencyInjection/TestServiceProvider.cs
+++ VSUnitTest.Desktop/Jasily.DependencyInjection/TestServiceProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Jasily.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -14,16 +15,19 @@ namespace VSUnitTest.Desktop.Jasily.DependencyInjection
             list.AddSingleton("key", "5");
             list.AddSingleton("value", 8);
             list.AddTransient<KeyValuePair<string, int>>(null);
-            var provider = ServiceProvider.Build(list);
-            var result = (ResolveResult) provider.GetService(typeof(KeyValuePair<string, int>));
-            Assert.IsNotNull(result);
-            Assert.IsTrue(result.HasValue);
-            var service = result.Value;
-            Assert.IsNotNull(service);
-            Assert.IsInstanceOfType(service, typeof(KeyValuePair<string, int>));
-            var kvp = (KeyValuePair<string, int>) service;
-            Assert.AreEqual("5", kvp.Key);
-            Assert.AreEqual(8, kvp.Value);
+            var provider = ServiceProvider.Build(list, new ServiceProviderSettings { EnableDebug = true });
+            foreach (var _ in Enumerable.Range(0, 10))
+            {
+                var result = (ResolveResult)provider.GetService(typeof(KeyValuePair<string, int>));
+                Assert.IsNotNull(result);
+                Assert.IsTrue(result.HasValue);
+                var service = result.Value;
+                Assert.IsNotNull(service);
+                Assert.IsInstanceOfType(service, typeof(KeyValuePair<string, int>));
+                var kvp = (KeyValuePair<string, int>)service;
+                Assert.AreEqual("5", kvp.Key);
+                Assert.AreEqual(8, kvp.Value);
+            }
         }
     }
 }
