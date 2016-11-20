8630b2b970e30dc0db7135463112b3698551c824
赖巍 <skyoflw@gmail.com>
2016-11-20 21:29:18 +0800

    test for di.

---
+++ VSUnitTest.Desktop/Jasily.DependencyInjection/TestServiceProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Jasily.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSUnitTest.Desktop.Jasily.DependencyInjection
+{
+    [TestClass]
+    public class TestServiceProvider
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var list = ServiceProvider.CreateServiceCollection();
+            list.AddSingleton("key", "5");
+            list.AddSingleton("value", 8);
+            list.AddTransient<KeyValuePair<string, int>>(null);
+            var provider = ServiceProvider.Build(list);
+            var result = (ResolveResult) provider.GetService(typeof(KeyValuePair<string, int>));
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.HasValue);
+            var service = result.Value;
+            Assert.IsNotNull(service);
+            Assert.IsInstanceOfType(service, typeof(KeyValuePair<string, int>));
+            var kvp = (KeyValuePair<string, int>) service;
+            Assert.AreEqual("5", kvp.Key);
+            Assert.AreEqual(8, kvp.Value);
+        }
+    }
+}
