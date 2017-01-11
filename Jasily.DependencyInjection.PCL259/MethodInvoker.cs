8d3991b89bfac8f7d46c879fa1a64eb13ffabf96
赖巍 <skyoflw@gmail.com>
2017-01-11 10:56:21 +0800

    impl invoker.

--- Jasily.DependencyInjection.PCL259/MethodInvoker.cs
+++ Jasily.DependencyInjection.PCL259/MethodInvoker.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using System.Runtime.ExceptionServices;
 using JetBrains.Annotations;
 
 namespace Jasily.DependencyInjection
@@ -11,20 +12,50 @@ namespace Jasily.DependencyInjection
         public MethodInvoker([NotNull] ServiceProvider provider)
         {
             if (provider == null) throw new ArgumentNullException(nameof(provider));
-            this.provider = provider;
+            this.provider = provider.CreateScope();
         }
 
         public void Invoke<T>(T obj, [NotNull] MethodInfo methodInfo)
         {
             if (methodInfo == null) throw new ArgumentNullException(nameof(methodInfo));
-            throw new NotImplementedException();
+            this.InternalInvoke(obj, methodInfo);
         }
 
         public TResult Invoke<T, TResult>(T obj, [NotNull] MethodInfo methodInfo)
         {
             if (methodInfo == null) throw new ArgumentNullException(nameof(methodInfo));
             if (methodInfo.ReturnType == typeof(void)) throw new InvalidOperationException("method has no return value.");
-            throw new NotImplementedException();
+            return (TResult) this.InternalInvoke(obj, methodInfo);
+        }
+
+        public object InternalInvoke(object obj, [NotNull] MethodInfo methodInfo)
+        {
+            var parameters = this.ResolveParameters(methodInfo);
+            object value;
+            try
+            {
+                value = methodInfo.Invoke(obj, parameters);
+            }
+            catch (TargetInvocationException e)
+            {
+                ExceptionDispatchInfo.Capture(e.InnerException).Throw();
+                throw;
+            }
+            return value;
+        }
+
+        private object[] ResolveParameters(MethodBase methodInfo)
+        {
+            var parameters = methodInfo.GetParameters();
+            var parameterValues = new object[parameters.Length];
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                var parameter = parameters[i];
+                var result = this.provider.GetService(parameter.ParameterType, parameter.Name);
+                if (!result.HasValue) throw new InvalidOperationException();
+                parameterValues[i] = result.Value;
+            }
+            return parameterValues;
         }
     }
 }
