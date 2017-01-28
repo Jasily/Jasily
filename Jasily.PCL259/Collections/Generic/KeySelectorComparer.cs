3b992b7f409eb73f7f997a9300fe16256fd5b158
赖巍 <skyoflw@gmail.com>
2017-01-29 04:02:16 +0800

    refactor.

--- Jasily.PCL259/Collections/Generic/KeySelectorComparer.cs
+++ Jasily.PCL259/Collections/Generic/KeySelectorComparer.cs
@@ -4,24 +4,6 @@ using JetBrains.Annotations;
 
 namespace Jasily.Collections.Generic
 {
-    public class KeySelectorComparer
-    {
-        public static KeySelectorComparer<TItem, TKey> Create<TItem, TKey>([NotNull] Func<TItem, TKey> keySelector)
-            => new KeySelectorComparer<TItem, TKey>(keySelector);
-
-        public static KeySelectorComparer<T, string> Create<T>([NotNull] Func<T, string> keySelector,
-            [NotNull] StringComparer comparer)
-            => new StringKeySelectorComparer<T>(keySelector, comparer);
-
-        private class StringKeySelectorComparer<T> : KeySelectorComparer<T, string>
-        {
-            public StringKeySelectorComparer([NotNull] Func<T, string> keySelector, [NotNull] StringComparer comparer)
-                : base(keySelector, comparer, comparer)
-            {
-            }
-        }
-    }
-
     public class KeySelectorComparer<TItem, TKey> : IComparer<TItem>, IEqualityComparer<TItem>
     {
         private readonly Func<TItem, TKey> keySelector;
@@ -29,19 +11,17 @@ namespace Jasily.Collections.Generic
         private readonly IEqualityComparer<TKey> equalityComparer; 
 
         public KeySelectorComparer([NotNull] Func<TItem, TKey> keySelector)
-            : this(keySelector, Comparer<TKey>.Default, EqualityComparer<TKey>.Default)
+            : this(keySelector, null, null)
         {
         }
 
         protected KeySelectorComparer([NotNull] Func<TItem, TKey> keySelector,
-            [NotNull] IComparer<TKey> comparer, [NotNull] IEqualityComparer<TKey> equalityComparer)
+            [CanBeNull] IComparer<TKey> comparer, [CanBeNull] IEqualityComparer<TKey> equalityComparer)
         {
             if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
-            if (comparer == null) throw new ArgumentNullException(nameof(comparer));
-            if (equalityComparer == null) throw new ArgumentNullException(nameof(equalityComparer));
             this.keySelector = keySelector;
-            this.comparer = comparer;
-            this.equalityComparer = equalityComparer;
+            this.comparer = comparer ?? Comparer<TKey>.Default;
+            this.equalityComparer = equalityComparer ?? EqualityComparer<TKey>.Default;
         }
 
         public int Compare(TItem x, TItem y)
---
+++ Jasily.PCL259/KeySelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Jasily.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Jasily
+{
+    public class KeySelector
+    {
+        #region comparer
+
+        public static KeySelectorComparer<TItem, TKey> CreateComparer<TItem, TKey>([NotNull] Func<TItem, TKey> keySelector)
+           => new KeySelectorComparer<TItem, TKey>(keySelector);
+
+        public static KeySelectorComparer<T, string> CreateComparer<T>([NotNull] Func<T, string> keySelector,
+            [NotNull] StringComparer comparer)
+            => new StringKeySelectorComparer<T>(keySelector, comparer);
+
+        private class StringKeySelectorComparer<T> : KeySelectorComparer<T, string>
+        {
+            public StringKeySelectorComparer([NotNull] Func<T, string> keySelector, [NotNull] StringComparer comparer)
+                : base(keySelector, comparer, comparer)
+            {
+            }
+        }
+
+        #endregion
+    }
+}
