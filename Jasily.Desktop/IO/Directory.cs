0d0c50be02c54acf2d0b637abbdc7702c3a6b4b0
赖巍 <skyoflw@gmail.com>
2017-02-18 18:57:01 +0800

    added ensure directory method.

---
+++ Jasily.Desktop/IO/Directory.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Jasily.IO
+{
+    using SIO = System.IO;
+
+    public static class Directory
+    {
+        public static void EnsureParentDirectory(string path)
+        {
+            var dir = SIO.Path.GetDirectoryName(path);
+            if (!string.IsNullOrEmpty(dir)) EnsureDirectory(dir);
+        }
+
+        public static void EnsureDirectory(string path)
+        {
+            if (SIO.Directory.Exists(path)) return;
+            var parent = SIO.Path.GetDirectoryName(path);
+            if (!string.IsNullOrEmpty(parent)) EnsureDirectory(parent);
+            SIO.Directory.CreateDirectory(path);
+        }
+    }
+}
