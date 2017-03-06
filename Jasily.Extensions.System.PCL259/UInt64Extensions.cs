e6547ac55280b669db8f69e879e9c0760474114e
赖巍 <skyoflw@gmail.com>
2017-03-06 09:49:52 +0800

    continue for sugar

---
+++ Jasily.Extensions.System.PCL259/UInt64Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System
+{
+    public static class UInt64Extensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint OfLower(this ulong value) => (uint)(value & uint.MaxValue);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint OfHigh(this ulong value) => (uint)(value >> 32);
+    }
+}
