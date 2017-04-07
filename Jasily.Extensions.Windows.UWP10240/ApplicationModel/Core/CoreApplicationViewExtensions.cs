a13c5595f4cf5d63173c906d29e9a99ea54296c4
赖巍 <skyoflw@gmail.com>
2017-04-07 19:29:53 +0800

    replace api.

---
+++ Jasily.Extensions.Windows.UWP10240/ApplicationModel/Core/CoreApplicationViewExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.ViewManagement;
+using JetBrains.Annotations;
+
+namespace Windows.ApplicationModel.Core
+{
+    public static class CoreApplicationViewExtensions
+    {
+        public static DataTransferManager GetDataTransferManager([NotNull] CoreApplicationView view)
+        {
+            if (view == null) throw new ArgumentNullException(nameof(view));
+            return DataTransferManager.GetForCurrentView();
+        }
+
+        public static ApplicationView GetApplicationView([NotNull] CoreApplicationView view)
+        {
+            if (view == null) throw new ArgumentNullException(nameof(view));
+            return ApplicationView.GetForCurrentView();
+        }
+    }
+}
