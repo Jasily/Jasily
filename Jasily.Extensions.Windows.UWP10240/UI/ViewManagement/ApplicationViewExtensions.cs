93baa266ef6b7a75e923e7cdfab197498d79f107
赖巍 <skyoflw@gmail.com>
2017-04-07 18:59:17 +0800

    add api.

---
+++ Jasily.Extensions.Windows.UWP10240/UI/ViewManagement/ApplicationViewExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Windows.ApplicationModel.DataTransfer;
+using JetBrains.Annotations;
+
+namespace Windows.UI.ViewManagement
+{
+    public static class ApplicationViewExtensions
+    {
+        public static DataTransferManager GetDataTransferManager([NotNull] ApplicationView view)
+        {
+            if (view == null) throw new ArgumentNullException(nameof(view));
+            return DataTransferManager.GetForCurrentView();
+        }
+    }
+}
--- Jasily.UWP10240/SystemInfo.cs
+++ Jasily.UWP10240/SystemInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.System.Profile;
 
 namespace Jasily
