a7171b34bb1860070d92dae0d983280052f47f07
赖巍 <skyoflw@gmail.com>
2017-04-30 23:24:15 +0800

    add NoyNull extension method for enumerable

---
+++ Jasily.Extensions.System.PCL259/Linq/JEnumerable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace System.Linq
+{
+    /// <summary>
+    /// extension methods for <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static partial class JEnumerable
+    {
+        /// <summary>
+        /// filter by whether reference equals null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <exception cref="ArgumentNullException">throw if <paramref name="source"/> is null.</exception>
+        /// <returns></returns>
+        public static IEnumerable<T> NotNull<T>([NotNull] this IEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            IEnumerable<T> Iterator()
+            {
+                foreach (var item in source)
+                {
+                    if (!ReferenceEquals(item, null)) yield return item;
+                }
+            }
+
+            return Iterator();
+        }
+    }
+}
