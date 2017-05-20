1a9094d64baf853d7d08b5db63805788db987a87
赖巍 <skyoflw@gmail.com>
2017-05-20 13:22:37 +0800

    add test class.

--- VSUnitTest.Desktop/DependencyInjection.MethodInvoker/TestMethodInvoker.cs
+++ VSUnitTest.Desktop/DependencyInjection.MethodInvoker/TestMethodInvoker.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -80,6 +81,68 @@ namespace Jasily.DependencyInjection.MethodInvoker
             Assert.AreEqual(null, factory.InvokeInstanceMethod(method, new Class1(), provider, args));
             method = typeof(Class1).GetRuntimeMethods().Single(z => z.Name == nameof(Class1.Method8));
             Assert.AreEqual(null, factory.InvokeStaticMethod(method, provider, args));
+        }
+
+        public abstract class Class2
+        {
+            public abstract int AbstractMethod();
+
+            public virtual int VirutualMethod() => 1;
+        }
+
+        public class Class2SubClass : Class2
+        {
+            public override int AbstractMethod() => 2;
+
+            public override int VirutualMethod() => 2;
+
+            public override string ToString() => "2";
+        }
+
+        public class Class2SubClassSubClass : Class2SubClass
+        {
+            public override int AbstractMethod() => 3;
+
+            public override int VirutualMethod() => 3;
+        }
+
+        [TestMethod]
+        public void TestInheritMethod()
+        {
+            var sc = new ServiceCollection();
+            sc.UseMethodInvoker();
+            var provider = sc.BuildServiceProvider();
+
+            void AssertExpected<T>(IMethodInvokerFactory factory, T instance, object value1, object value2, object value3)
+            {
+                Assert.IsNotNull(factory);
+
+                MethodInfo method;
+                IObjectMethodInvoker invoker;
+
+                method = typeof(T).GetRuntimeMethods().Single(z => z.Name == nameof(Class2SubClass.AbstractMethod));
+                invoker = factory.GetObjectMethodInvoker(method);
+                Assert.AreEqual(value1, invoker.Invoke(instance, provider));
+
+                method = typeof(T).GetRuntimeMethods().Single(z => z.Name == nameof(Class2.VirutualMethod));
+                invoker = factory.GetObjectMethodInvoker(method);
+                Assert.AreEqual(value2, invoker.Invoke(instance, provider));
+
+                method = typeof(T).GetRuntimeMethods().Single(z => z.Name == nameof(Class2.ToString));
+                invoker = factory.GetObjectMethodInvoker(method);
+                Assert.AreEqual(value3, invoker.Invoke(instance, provider));
+            }
+
+            var obj1 = new Class2SubClass();
+            var f = (IMethodInvokerFactory) provider.GetService<IMethodInvokerFactory<Class2SubClass>>();
+            AssertExpected<Class2>(f, obj1, 2, 2, "2");
+            AssertExpected<Class2SubClass>(f, obj1, 2, 2, "2");
+
+            var obj2 = new Class2SubClassSubClass();
+            f = provider.GetService<IMethodInvokerFactory<Class2SubClassSubClass>>();
+            AssertExpected<Class2>(f, obj2, 3, 3, "2");
+            AssertExpected<Class2SubClass>(f, obj2, 3, 3, "2");
+            AssertExpected<Class2SubClassSubClass>(f, obj2, 3, 3, "2");
         }
     }
 }
