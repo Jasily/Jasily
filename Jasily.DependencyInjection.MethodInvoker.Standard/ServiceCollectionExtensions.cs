da9d2b78ccfbbc9572a566c8bdea2e8e8c0fbe02
赖巍 <skyoflw@gmail.com>
2017-05-21 15:57:40 +0800

    rename api.

--- Jasily.DependencyInjection.MethodInvoker.Standard/ServiceCollectionExtensions.cs
+++ Jasily.DependencyInjection.MethodInvoker.Standard/ServiceCollectionExtensions.cs
@@ -16,7 +16,7 @@ namespace Jasily.DependencyInjection.MethodInvoker
         /// </summary>
         /// <param name="serviceCollection"></param>
         /// <exception cref="ArgumentNullException">throw if <paramref name="serviceCollection"/> is null.</exception>
-        public static void UseMethodInvoker([NotNull] this IServiceCollection serviceCollection)
+        public static void AddMethodInvoker([NotNull] this IServiceCollection serviceCollection)
         {
             if (serviceCollection == null) throw new ArgumentNullException(nameof(serviceCollection));
 
@@ -25,6 +25,17 @@ namespace Jasily.DependencyInjection.MethodInvoker
             serviceCollection.AddSingleton(typeof(IMethodInvokerFactory<>), typeof(MethodInvokerFactory<>));
             serviceCollection.AddSingleton(typeof(ISingletonArguments<>), typeof(ConcurrentArguments<>));
             serviceCollection.AddScoped(typeof(IScopedArguments<>), typeof(ConcurrentArguments<>));
+        }
+
+        /// <summary>
+        /// add <see cref="MethodInvoker"/> services to <paramref name="serviceCollection"/>.
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <exception cref="ArgumentNullException">throw if <paramref name="serviceCollection"/> is null.</exception>
+        [Obsolete("use `AddMethodInvoker`")]
+        public static void UseMethodInvoker([NotNull] this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMethodInvoker();
         }
     }
 }
