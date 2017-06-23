535ded5c63290df4d1f7b5aef3d7098d27ee0262
赖巍 <skyoflw@gmail.com>
2017-06-24 01:56:55 +0800

    add api for save property.

--- Jasily/ComponentModel/ViewModel.cs
+++ Jasily/ComponentModel/ViewModel.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 
 namespace Jasily.ComponentModel
 {
     public class ViewModel : NotifyPropertyChangedObject
     {
+        private Dictionary<string, object> _properties;
+
+        /// <summary>
+        /// Get property from internal dictionary. 
+        /// This method may unboxing <see cref="ValueType"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        [InvokeOn(ThreadKind.UIThread)]
+        protected T GetProperty<T>([NotNull, CallerMemberName] string propertyName = "")
+        {
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+            return this._properties != null && this._properties.TryGetValue(propertyName, out var val) ? (T) val : default(T);
+        }
+
+        /// <summary>
+        /// Set property to internal dictionary.
+        /// This method may boxing <see cref="ValueType"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        [InvokeOn(ThreadKind.UIThread)]
+        protected bool SetProperty<T>(T value, [NotNull, CallerMemberName] string propertyName = "")
+        {
+            if (this._properties == null)
+            {
+                this._properties = new Dictionary<string, object>();
+            }
+            else if (this._properties.TryGetValue(propertyName, out var val) && EqualityComparer<T>.Default.Equals(value, (T)val))
+            {
+                return false;
+            }
+
+            this._properties[propertyName] = value;
+            return true;
+        }
     }
 
     public class ViewModel<TSource> : ViewModel
