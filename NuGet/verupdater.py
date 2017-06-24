6a2e7340e4c4ab9ef4b57b47ceb65da8ab018967
赖巍 <skyoflw@gmail.com>
2017-06-24 23:27:00 +0800

    add confirm.

--- NuGet/verupdater.py
+++ NuGet/verupdater.py
@@ -3,7 +3,8 @@
 #
 # Copyright (c) 2017 - cologler <skyoflw@gmail.com>
 # ----------
-#
+# HOW TO USE: e.g:
+# verupdater.py D:\Projects\jasily-csharp\Jasily.Standard\Jasily.Standard.nuspec
 # ----------
 
 import os
@@ -21,12 +22,28 @@ def detect_proj(proj_id):
     return proj_id.lower().startswith('jasily')
 
 
+class PackageXml:
+    def __init__(self, path):
+        if not os.path.isfile(path):
+            raise Exception('missing %s' % path)
+        self._path = path
+        self._xml_model = None
+
+    def load(self):
+        self._xml_model = ET.parse(path)
+        return self._xml_model
+
+    def save(self):
+        self._xml_model.write(self._path, xml_declaration=True, encoding='utf-8')
+
+
 class VersionUpdater:
     def __init__(self, script_path):
         script_dir = os.path.dirname(script_path)
         self._verfile = os.path.join(script_dir, 'version.txt')
         self._sln_root = os.path.dirname(script_dir)
         self._version = self.read_version()
+        self._xmls = []
 
     def read_version(self):
         with open(self._verfile) as fp:
@@ -43,9 +60,11 @@ class VersionUpdater:
         if os.path.splitext(path)[1].lower() != '.nuspec':
             raise Exception
         self._internal_incr_version(path)
+        self.confirm()
 
     def _internal_incr_version(self, path):
-        xml_model = ET.parse(path)
+        p = PackageXml(path)
+        xml_model = p.load()
         package = xml_model.getroot()
         metadata = package.find('ns:metadata', NS)
         version = metadata.find('ns:version', NS)
@@ -62,7 +81,13 @@ class VersionUpdater:
                 for group in dependency_root.findall('ns:group', NS):
                     visit_dependency(group)
             visit_dependency(dependencies)
-        xml_model.write(path, xml_declaration=True, encoding='utf-8')
+        self._xmls.append(p)
+
+    def confirm(self):
+        print('press any key to update version to %s' % self._version)
+        input()
+        for item in self._xmls:
+            item.save()
 
 
 def main(argv=None):
