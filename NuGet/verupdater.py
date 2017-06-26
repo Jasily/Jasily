cd8aabd4dfedef48690177d688e3a3b38ecd2178
赖巍 <skyoflw@gmail.com>
2017-06-26 14:11:36 +0800

    update script.

--- NuGet/verupdater.py
+++ NuGet/verupdater.py
@@ -18,23 +18,103 @@ NS = { 'ns': 'http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd' }
 ET.register_namespace('', 'http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd')
 
 
-def detect_proj(proj_id):
-    return proj_id.lower().startswith('jasily')
+class AppVersion:
+    def __init__(self, *args):
+        if len(args) == 0:
+            raise ValueError('miss args.')
+        self._vers = []
+        if len(args) == 1:
+            if isinstance(args[0], str):
+                self.parse_from_string(args[0])
+            elif isinstance(args[0], AppVersion):
+                self._vers = args[0]._vers.copy()
+            else:
+                raise ValueError
+        else:
+            self.parse_from_vers(args)
+
+    def parse_from_string(self, s):
+        fp = '(\d+)'
+        op = r'(?:\.(\d+))?'
+        m = re.match('^{fp}{op}{op}{op}$'.format(fp=fp, op=op), s)
+        if not m:
+            raise NotImplementedError
+        for n in m.groups():
+            if n != None:
+                v = int(n)
+                self._vers.append(v)
+
+    def parse_from_vers(self, ns):
+        for n in ns:
+            if not isinstance(n, int):
+                raise ValueError
+            self._vers.append(n)
+
+    def __str__(self):
+        return '.'.join([str(x) for x in self._vers])
+
+    def __gt__(self, other):
+        return self.compare(other) > 0
+
+    def __lt__(self, other):
+        return self.compare(other) < 0
+
+    def __eq__(self, other):
+        return self.compare(other) == 0
+
+    def compare(self, other):
+        '''
+        return - if self < other;
+        return 0 if self == other;
+        return + if self > other;
+        '''
+        if not isinstance(other, AppVersion):
+            raise NotImplementedError
+        c1 = min(len(self._vers), len(other._vers))
+        for i in range(0, c1):
+            l = self._vers[i]
+            r = other._vers[i]
+            if l != r:
+                return l - r
+        return len(self._vers) - len(other._vers)
+
+    def incr(self):
+        self._vers[-1] += 1
+        return self
 
 
 class PackageXml:
     def __init__(self, path):
         if not os.path.isfile(path):
             raise Exception('missing %s' % path)
+
         self._path = path
-        self._xml_model = None
+
+        self._xml_root = ET.parse(self._path)
+        self._x_package = self._xml_root.getroot()
+        self._x_metadata = self._x_package.find('ns:metadata', NS)
+        self._x_version = self._x_metadata.find('ns:version', NS)
+
+        self._version = AppVersion(self._x_version.text)
 
     def __str__(self):
         return os.path.split(self._path)[1]
 
+    @property
+    def package(self):
+        return self._x_package
+
+    @property
+    def version(self):
+        return AppVersion(self._version)
+
+    @version.setter
+    def version(self, value):
+        self._version = AppVersion(value)
+        self._x_version.text = str(value)
+
     def load(self):
-        self._xml_model = ET.parse(self._path)
-        return self._xml_model
+        return self._xml_root
 
     def save(self):
         self._xml_model.write(self._path, xml_declaration=True, encoding='utf-8')
@@ -48,15 +128,15 @@ class VersionUpdater:
         self._version = self.read_version()
         self._xmls = []
 
+    def on_detect_proj(self, proj_id):
+        return False
+
     def read_version(self):
         with open(self._verfile) as fp:
-            ver = fp.read()
-        m = re.match(r'^(\d)+\.(\d)+\.(\d)+\.(\d)+$', ver)
-        vs = list(m.groups())
-        vs[-1] = str(int(vs[-1]) + 1)
-        ver = '.'.join(vs)
+            ver = AppVersion(fp.read())
+        ver.incr()
         with open(self._verfile, 'w') as fp:
-            fp.write(ver)
+            fp.write(str(ver))
         return ver
 
     def resolve_version(self, path):
@@ -67,27 +147,25 @@ class VersionUpdater:
 
     def _internal_incr_version(self, path):
         p = PackageXml(path)
-        xml_model = p.load()
-        package = xml_model.getroot()
+        package = p.package
         metadata = package.find('ns:metadata', NS)
-        version = metadata.find('ns:version', NS)
-        version.text = self._version
+        p.version = self._version
         dependencies = metadata.find('ns:dependencies', NS)
         if dependencies:
             def visit_dependency(dependency_root):
                 for dependency in dependency_root.findall('ns:dependency', NS):
                     subproj = dependency.attrib['id']
-                    if detect_proj(subproj):
+                    if self.on_detect_proj(subproj):
                         subproj_nuspec = os.path.join(self._sln_root, subproj, subproj + '.nuspec')
                         self._internal_incr_version(subproj_nuspec)
-                        dependency.attrib['version'] = self._version
+                        dependency.attrib['version'] = str(self._version)
                 for group in dependency_root.findall('ns:group', NS):
                     visit_dependency(group)
             visit_dependency(dependencies)
         self._xmls.append(p)
 
     def confirm(self):
-        print('press any key to update version to %s:' % self._version)
+        print('press any key to update version to %s:' % str(self._version))
         for pkg in self._xmls:
             print('   ' + str(pkg))
         input()
@@ -95,11 +173,16 @@ class VersionUpdater:
             item.save()
 
 
+class JasilyVersionUpdater(VersionUpdater):
+    def on_detect_proj(self, proj_id):
+        return proj_id.lower().startswith('jasily')
+
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     try:
-        updater = VersionUpdater(argv[0])
+        updater = JasilyVersionUpdater(argv[0])
         updater.resolve_version(argv[1])
     except Exception:
         traceback.print_exc()
