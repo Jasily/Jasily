cc09e28f9111cfee082bc86017d4f2e75faec596
赖巍 <skyoflw@gmail.com>
2017-06-29 16:12:48 +0800

    refactor.

--- Jasily.Extensions.System/Threading/Tasks/TaskExtensions.cs
+++ Jasily.Extensions.System/Threading/Tasks/TaskExtensions.cs
@@ -4,14 +4,26 @@ using JetBrains.Annotations;
 
 namespace Jasily.Extensions.System.Threading.Tasks
 {
+    /// <summary>
+    /// Extension methods for <see cref="Task"/>
+    /// </summary>
     public static class TaskExtensions
     {
+        /// <summary>
+        /// Ensure task start. If not, start it.
+        /// </summary>
+        /// <param name="task"></param>
         public static void EnsureStarted([NotNull] this Task task)
         {
             if (task == null) throw new ArgumentNullException(nameof(task));
             if (task.Status == TaskStatus.Created) task.Start();
         }
 
+        /// <summary>
+        /// Ensure task start. If not, start it.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <param name="scheduler"></param>
         public static void EnsureStarted([NotNull] this Task task, [NotNull] TaskScheduler scheduler)
         {
             if (task == null) throw new ArgumentNullException(nameof(task));
@@ -20,52 +32,38 @@ namespace Jasily.Extensions.System.Threading.Tasks
         }
 
         /// <summary>
-        /// return await Task.Run(async () => selector(await task));
+        /// return selector(<see langword="await"/> task.ConfigureAwait(<see langword="false"/>));
         /// </summary>
-        /// <typeparam name="TFrom"></typeparam>
-        /// <typeparam name="TTo"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <typeparam name="TNewResult"></typeparam>
         /// <param name="task"></param>
         /// <param name="selector"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public static async ValueTask<TTo> SelectAsync<TFrom, TTo>([NotNull] this Task<TFrom> task,
-            [NotNull] Func<TFrom, TTo> selector)
+        public static async Task<TNewResult> SelectAsync<TResult, TNewResult>([NotNull] this Task<TResult> task,
+            [NotNull] Func<TResult, TNewResult> selector)
         {
             if (task == null) throw new ArgumentNullException(nameof(task));
             if (selector == null) throw new ArgumentNullException(nameof(selector));
-            return await Task.Run(async () => 
-                selector(await task.ConfigureAwait(false))
-            ).ConfigureAwait(false);
+            return selector(await task.ConfigureAwait(false));
         }
 
         /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="TFrom"></typeparam>
-        /// <typeparam name="TTo"></typeparam>
-        /// <param name="task"></param>
-        /// <param name="asyncSelector"></param>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <returns></returns>
-        public static async ValueTask<TTo> AsyncSelectAsync<TFrom, TTo>([NotNull] this Task<TFrom> task,
-            [NotNull] Func<TFrom, Task<TTo>> asyncSelector)
-        {
-            if (task == null) throw new ArgumentNullException(nameof(task));
-            if (asyncSelector == null) throw new ArgumentNullException(nameof(asyncSelector));
-            return await Task.Run(async () =>
-                await asyncSelector(await task.ConfigureAwait(false)).ConfigureAwait(false)
-            ).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// create <see cref="ValueTask{TResult}"/> from <paramref name="value"/>.
+        /// return <see langword="await"/> taskSelector(<see langword="await"/> 
+        /// task.ConfigureAwait(<see langword="false"/>)).ConfigureAwait(<see langword="false"/>);
         /// </summary>
         /// <typeparam name="TResult"></typeparam>
-        /// <param name="value"></param>
+        /// <typeparam name="TNewResult"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="taskSelector"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         /// <returns></returns>
-        public static ValueTask<TResult> AsTaskResult<TResult>([CanBeNull] this TResult value)
+        public static async Task<TNewResult> SelectTaskAsync<TResult, TNewResult>([NotNull] this Task<TResult> task,
+            [NotNull] Func<TResult, Task<TNewResult>> taskSelector)
         {
-            return new ValueTask<TResult>(value);
+            if (task == null) throw new ArgumentNullException(nameof(task));
+            if (taskSelector == null) throw new ArgumentNullException(nameof(taskSelector));
+            return await taskSelector(await task.ConfigureAwait(false)).ConfigureAwait(false);
         }
     }
 }
