b73d389fe37a857406c84fe30df6d61355f1d993
skyoflw <skyoflw@gmail.com>
2017-10-06 13:04:37 +0800

    add GenericMakers benchmarks test

---
+++ VSUnitTest.Desktop/GenericMakers/TestGenericMaker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Jasily.TestUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jasily.GenericMakers
+{
+    [TestClass]
+    public class TestGenericMaker
+    {
+        [TestMethod, TestCategory("Benchmarks")]
+        public void PerformanceTest()
+        {
+            var maker = new GenericTypeMaker(typeof(Dictionary<,>));
+
+            CodeTimerHelper.FastThen(
+                () => maker.MakeGenericType(typeof(int), typeof(string)),
+                () => typeof(Dictionary<,>).MakeGenericType(typeof(int), typeof(string)),
+                1000000);
+        }
+    }
+}
