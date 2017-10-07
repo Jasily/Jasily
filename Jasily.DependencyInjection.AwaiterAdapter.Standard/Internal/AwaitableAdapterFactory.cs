219cab11d3384bc7d32740c0f44481ec9c3cb864
skyoflw <skyoflw@gmail.com>
2017-10-07 10:36:47 +0800

    refactor: use lazy

--- Jasily.DependencyInjection.AwaiterAdapter.Standard/Internal/AwaitableAdapterFactory.cs
+++ Jasily.DependencyInjection.AwaiterAdapter.Standard/Internal/AwaitableAdapterFactory.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Threading;
 using Jasily.DependencyInjection.MethodInvoker;
-using Jasily.Extensions.System.Collections.Concurrent;
-using Jasily.Extensions.System.Reflection;
 using JetBrains.Annotations;
 
 namespace Jasily.DependencyInjection.AwaiterAdapter.Internal
@@ -11,45 +10,20 @@ namespace Jasily.DependencyInjection.AwaiterAdapter.Internal
     internal class AwaitableAdapterFactory : IAwaitableAdapterFactory
     {
         private readonly IServiceProvider _serviceProvider;
-        private readonly ConcurrentDictionary<Type, IAwaitableAdapter> _adapters = new ConcurrentDictionary<Type, IAwaitableAdapter>();
-        private readonly Func<IAwaitableAdapter, bool> _isLockerFunc = o => o is Locker;
-        private readonly Func<IAwaitableAdapter> _lockerFactory = () => new Locker();
-        private readonly Func<Type, IAwaitableAdapter> _valueFactory;
+        private readonly ConcurrentDictionary<Type, Lazy<IAwaitableAdapter>> _adapters = new ConcurrentDictionary<Type, Lazy<IAwaitableAdapter>>();
+        private readonly Func<Type, Lazy<IAwaitableAdapter>> _valueFactory;
 
         public AwaitableAdapterFactory([NotNull] IServiceProvider serviceProvider)
         {
             this._serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
-            this._valueFactory = this.CreateAwaitableAdapter;
+            this._valueFactory = t => new Lazy<IAwaitableAdapter>(() => this.CreateAwaitableAdapter(t), LazyThreadSafetyMode.ExecutionAndPublication);
         }
 
         public IAwaitableAdapter GetAwaitableAdapter([NotNull] Type instanceType)
         {
             if (instanceType == null) throw new ArgumentNullException(nameof(instanceType));
 
-            return this._adapters.FastGetOrAdd(instanceType,
-                this._isLockerFunc,
-                this._lockerFactory,
-                this._valueFactory);
-        }
-
-        private class Locker : IAwaitableAdapter
-        {
-            public bool IsAwaitable { get; }
-
-            public bool IsCompleted(object instance)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object GetResult(object instance)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void OnCompleted(object instance, Action continuation)
-            {
-                throw new NotImplementedException();
-            }
+            return this._adapters.GetOrAdd(instanceType, this._valueFactory).Value;
         }
 
         [NotNull]
